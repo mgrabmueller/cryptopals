6a804e067a693d20a73187fc9a26d738c0ba7ee8
Martin Grabmueller <martin@grabmueller.de>
2016-10-06 19:26:48 +0200

    Start challenge 1.6.

---
+++ src/bin/challenge_1_6.rs
@@ -0,0 +1,13 @@
+// Copyright 2016 Martin Grabmueller. See the LICENSE file at the
+// top-level directory of this distribution for license information.
+
+extern crate cryptopals;
+
+use std::io::BufReader;
+use std::io::BufRead;
+use std::fs::File;
+
+use cryptopals::{codec, xor};
+
+pub fn main() {
+}
